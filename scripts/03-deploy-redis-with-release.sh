9b84f7e2265c22d282bc29613ed2344d32a8b3cd
Juan Pablo Genovese <juanpgenovese@gmail.com>
2016-05-19 19:07:58 -0300

    Fix scripts

--- scripts/03-deploy-redis-with-release.sh
+++ scripts/03-deploy-redis-with-release.sh
@@ -5,7 +5,8 @@ set -e
 source variables.sh
 
 echo Bosh director lives on: $BOSH_DIRECTOR_IP
-echo Redis release is $CF_REDIS_RELEASE
+echo Redis release is $CF_REDIS_RELEASE_DIR/releases/$CF_REDIS_RELEASE
+echo Redis manifest is $CF_REDIS_MANIFEST
 
 echo Targeting Bosh Lite
 bosh target $BOSH_DIRECTOR_IP lite
@@ -13,10 +14,11 @@ bosh target $BOSH_DIRECTOR_IP lite
 echo Uploading stemcell
 bosh -q -n upload stemcell --skip-if-exists $BOSH_LITE_STEMCELL
 
-cd $RESOURCES
+git clone -q https://github.com/pivotal-cf/cf-redis-release.git $CF_REDIS_RELEASE_DIR
+cd $CF_REDIS_RELEASE_DIR
 
 echo Uploading release
-bosh -n upload release $CF_REDIS_RELEASE
+bosh -n upload release $CF_REDIS_RELEASE_DIR/releases/$CF_REDIS_RELEASE
 
 sed -i -- "s/director_uuid: REPLACE_ME/director_uuid: $BOSH_UUID/g" $CF_REDIS_MANIFEST
 sed -i -- "s/bosh-lite.com/$CF_DOMAIN/g" $CF_REDIS_MANIFEST
